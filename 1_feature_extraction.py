480c2185ad94b444187978f7750a005848acf567
Daldossi <108078501+Daldossi@users.noreply.github.com>
2023-06-05 22:10:36 +0200

    Add files via upload

--- 1_feature_extraction.py
+++ 1_feature_extraction.py
@@ -9,6 +9,15 @@ import os # to scan the directory
 from image_features import *
 
 
+def minmax_normalization(Xtrain, Xtest): 
+    """ Min-max scaling """ 
+    xmin = Xtrain.min(0) 
+    xmax = Xtrain.max(0) 
+    Xtrain = (Xtrain - xmin) / (xmax - xmin) 
+    Xtest = (Xtest - xmin) / (xmax - xmin) 
+    return Xtrain, Xtest  
+
+
 def process_directory(path, feature_f): 
     """ Extract the features of all the images in that particular order """
     all_features = [] 
@@ -51,53 +60,54 @@ print(classes)
 
 ## EXTRACT the features   
 
-Xtest_edh, Ytest_edh = process_directory("images/test", edge_direction_histogram) 
-# print("test_edh", X.shape, Y.shape) 
-datatest_edh = np.concatenate([Xtest_edh, Ytest_edh[:, None]], 1)
-np.savetxt("test_edh.txt.gz", datatest_edh) 
 Xtrain_edh, Ytrain_edh = process_directory("images/train", edge_direction_histogram) 
-# print("train_edh", X.shape, Y.shape) 
 datatrain_edh = np.concatenate([Xtrain_edh, Ytrain_edh[:, None]], 1) 
+Xtest_edh, Ytest_edh = process_directory("images/test", edge_direction_histogram) 
+datatest_edh = np.concatenate([Xtest_edh, Ytest_edh[:, None]], 1)
+datatrain_edh, datatest_edh = minmax_normalization(Xtrain_edh, Xtest_edh) 
 np.savetxt("train_edh.txt.gz", datatrain_edh) 
+np.savetxt("test_edh.txt.gz", datatest_edh) 
 
-Xtest_ch, Ytest_ch = process_directory("images/test", color_histogram) 
-# print("test_ch", X.shape, Y.shape) 
-datatest_ch = np.concatenate([Xtest_ch, Ytest_ch[:, None]], 1)
-np.savetxt("test_ch.txt.gz", datatest_ch) 
 Xtrain_ch, Ytrain_ch = process_directory("images/train", color_histogram) 
-# print("train_ch", X.shape, Y.shape) 
 datatrain_ch = np.concatenate([Xtrain_ch, Ytrain_ch[:, None]], 1) 
+Xtest_ch, Ytest_ch = process_directory("images/test", color_histogram) 
+datatest_ch = np.concatenate([Xtest_ch, Ytest_ch[:, None]], 1)
+datatrain_ch, datatest_ch = minmax_normalization(Xtrain_ch, Xtest_ch) 
 np.savetxt("train_ch.txt.gz", datatrain_ch) 
+np.savetxt("test_ch.txt.gz", datatest_ch) 
 
-Xtest_cm, Ytest_cm = process_directory("images/test", cooccurrence_matrix) 
-# print("test_cm", X.shape, Y.shape) 
-datatest_cm = np.concatenate([Xtest_cm, Ytest_cm[:, None]], 1)
-np.savetxt("test_cm.txt.gz", datatest_cm) 
 Xtrain_cm, Ytrain_cm = process_directory("images/train", cooccurrence_matrix) 
-# print("train_cm", X.shape, Y.shape) 
 datatrain_cm = np.concatenate([Xtrain_cm, Ytrain_cm[:, None]], 1) 
-np.savetxt("train_cm.txt.gz", datatrain_cm) 
+Xtest_cm, Ytest_cm = process_directory("images/test", cooccurrence_matrix) 
+datatest_cm = np.concatenate([Xtest_cm, Ytest_cm[:, None]], 1) 
+datatrain_cm, datatest_cm = minmax_normalization(Xtrain_cm, Xtest_cm) 
+np.savetxt("train_cm.txt.gz", datatrain_cm)
+np.savetxt("test_cm.txt.gz", datatest_cm)  
 
-Xtest_rcm, Ytest_rcm = process_directory("images/test", rgb_cooccurrence_matrix) 
-# print("test_rcm", X.shape, Y.shape) 
-datatest_rcm = np.concatenate([Xtest_rcm, Ytest_rcm[:, None]], 1)
-np.savetxt("test_rcm.txt.gz", datatest_rcm) 
 Xtrain_rcm, Ytrain_rcm = process_directory("images/train", rgb_cooccurrence_matrix) 
 # print("train_rcm", X.shape, Y.shape) 
 datatrain_rcm = np.concatenate([Xtrain_rcm, Ytrain_rcm[:, None]], 1) 
+Xtest_rcm, Ytest_rcm = process_directory("images/test", rgb_cooccurrence_matrix) 
+# print("test_rcm", X.shape, Y.shape) 
+datatest_rcm = np.concatenate([Xtest_rcm, Ytest_rcm[:, None]], 1)
+datatrain_rcm, datatest_rcm = minmax_normalization(Xtrain_rcm, Xtest_rcm) 
 np.savetxt("train_rcm.txt.gz", datatrain_rcm) 
+np.savetxt("test_rcm.txt.gz", datatest_rcm) 
 
-datatest_edh_ch = np.concatenate([Xtest_edh, Xtest_ch, Ytest_edh[:, None], Ytest_ch[:, None]], 1)
-np.savetxt("test_edh_ch.txt.gz", datatest_rcm) 
-datatrain_edh_ch = np.concatenate([Xtrain_edh, Xtrain_ch, Ytrain_edh[:, None], Ytrain_ch[:, None]], 1)
-np.savetxt("train_edh_ch.txt.gz", datatrain_rcm) 
+datatrain_edh_ch = np.concatenate([Xtrain_edh, Xtrain_ch, Ytrain_edh[:, None], Ytrain_ch[:, None]], 1) 
+datatest_edh_ch = np.concatenate([Xtest_edh, Xtest_ch, Ytest_edh[:, None], Ytest_ch[:, None]], 1) 
+# datatrain_edh_ch, datatest_edh_ch = minmax_normalization(datatrain_edh_ch, datatest_edh_ch) 
+np.savetxt("train_edh_ch.txt.gz", datatrain_edh_ch) 
+np.savetxt("test_edh_ch.txt.gz", datatest_edh_ch) 
 
-datatest_edh_ch = np.concatenate([Xtest_rcm, Xtest_ch, Ytest_rcm[:, None], Ytest_ch[:, None]], 1)
-np.savetxt("test_rcm_ch.txt.gz", datatest_rcm) 
-datatrain_edh_ch = np.concatenate([Xtrain_rcm, Xtrain_ch, Ytrain_rcm[:, None], Ytrain_ch[:, None]], 1)
-np.savetxt("train_rcm_ch.txt.gz", datatrain_rcm) 
+datatest_rcm_ch = np.concatenate([Xtest_rcm, Xtest_ch, Ytest_rcm[:, None], Ytest_ch[:, None]], 1)
+datatrain_rcm_ch = np.concatenate([Xtrain_rcm, Xtrain_ch, Ytrain_rcm[:, None], Ytrain_ch[:, None]], 1)
+# datatrain_rcm_ch, datatest_rcm_ch = minmax_normalization(datatrain_rcm_ch, datatest_rcm_ch) 
+np.savetxt("train_rcm_ch.txt.gz", datatrain_rcm_ch) 
+np.savetxt("test_rcm_ch.txt.gz", datatest_rcm_ch)
 
-datatest_edh_ch = np.concatenate([Xtest_edh, Xtest_ch, Xtest_cm, Ytest_edh[:, None], Ytest_ch[:, None], Ytest_cm[:, None]], 1)
-np.savetxt("test_edh_ch_cm.txt.gz", datatest_rcm) 
-datatrain_edh_ch = np.concatenate([Xtrain_edh, Xtrain_ch, Xtrain_cm, Ytrain_edh[:, None], Ytrain_ch[:, None], Ytrain_cm[:, None]], 1)
-np.savetxt("train_edh_ch_cm.txt.gz", datatrain_rcm) 
+datatest_edh_ch_cm = np.concatenate([Xtest_edh, Xtest_ch, Xtest_cm, Ytest_edh[:, None], Ytest_ch[:, None], Ytest_cm[:, None]], 1)
+datatrain_edh_ch_cm = np.concatenate([Xtrain_edh, Xtrain_ch, Xtrain_cm, Ytrain_edh[:, None], Ytrain_ch[:, None], Ytrain_cm[:, None]], 1)
+# datatrain_edh_ch_cm, datatest_edh_ch_cm = minmax_normalization(datatrain_edh_ch_cm, datatest_edh_ch_cm) 
+np.savetxt("train_edh_ch_cm.txt.gz", datatrain_edh_ch_cm) 
+np.savetxt("test_edh_ch_cm.txt.gz", datatest_edh_ch_cm) 
